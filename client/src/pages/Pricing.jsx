0daa032ab753be7796d4ea25359c80ba7dcf2647
raghav1321 <raghavkansal1321@gmail.com>
2026-07-18 17:46:01 +0530

    Update Pricing.jsx

--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -3,6 +3,7 @@ import { FaArrowLeft, FaCheckCircle } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import { motion } from "motion/react";
 import axios from 'axios';
+axios.defaults.withCredentials = true;
 import { ServerUrl } from '../App';
 import { useDispatch } from 'react-redux';
 import { setUserData } from '../redux/userSlice';
