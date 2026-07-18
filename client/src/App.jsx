743384c72af56dbde8a33116882ee32000a7d50b
raghav1321 <raghavkansal1321@gmail.com>
2026-07-18 14:49:52 +0530

    Updated ServerUrl

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://interviewiq-backend-h8sj.onrender.com"
+export const ServerUrl  = "https://interviewiq-backend1-7cd2.onrender.com"
 
 function App() {
 
