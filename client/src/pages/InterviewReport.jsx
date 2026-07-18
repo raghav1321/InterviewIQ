60b879323070a7f5e4584ce79a632e2ef87a060e
raghav1321 <raghavkansal1321@gmail.com>
2026-07-18 17:45:50 +0530

    Update InterviewReport.jsx

--- client/src/pages/InterviewReport.jsx
+++ client/src/pages/InterviewReport.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from "axios"
+axios.defaults.withCredentials = true;
 import { ServerUrl } from '../App';
 import Step3Report from '../components/Step3Report';
 function InterviewReport() {
